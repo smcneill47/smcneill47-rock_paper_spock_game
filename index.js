a5ce179dd6cbba30a84b2ab90d5cd0a77a07bbdd
smcneill47 <smcneill47@gmail.com>
2023-02-28 21:43:43 +0000

    add logic and switch to determine winner of game

--- index.js
+++ index.js
@@ -1,3 +1,81 @@
+// Returns a random number in the range of 1-5 for our computers chouce//
 function getComputerChoice() {
     return Math.floor((Math.random() * 5) + 1)
+}
+// sets variables for each of the users options 1-5// 
+const ROCK='ROCK';
+const SCISSORS="SCISSORS";
+const PAPER="PAPER";
+const SPOCK="SPOCK";
+const LIZARD="LIZARD";
+const OPTIONS= {
+    1: ROCK,
+    2: SCISSORS,
+    3: PAPER,
+    4: SPOCK, 
+    5: LIZARD 
+};
+
+//stores user input//
+let userChoiceID='';
+
+//event listener for when the page is loaded//
+window.onload = () => {
+    const userOptions = document.querySelectorAll('.user-option');
+
+    userOptions.forEach(el => el.addEventListener('click', event => {
+        userChoiceID = event.target.id;
+    }));
+
+    const playButton = document.querySelector('#play');
+
+    playButton.addEventListener('click', () => {
+        const computerChoiceID = getComputerChoice();
+        const computerChoice = OPTIONS[computerChoiceID];
+        const computerChoiceElement = document.querySelector('#computer-choice');
+        const output = document.querySelector('#output');
+        const userChoice = OPTIONS[userChoiceID];
+
+        let result = '';
+
+        computerChoiceElement.innerHTML = OPTIONS[computerChoiceID];
+
+        switch (`${computerChoice}-${userChoice}`) {
+            case `${ROCK}-${ROCK}`:
+            case `${SCISSORS}-${SCISSORS}`:
+            case `${PAPER}-${PAPER}`:
+            case `${SPOCK}-${SPOCK}`:
+            case `${LIZARD}-${LIZARD}`:
+                result = 'TIE'
+                break;
+            case `${ROCK}-${SCISSORS}`:
+            case `${ROCK}-${LIZARD}`:
+            case `${SCISSORS}-${PAPER}`:
+            case `${SCISSORS}-${LIZARD}`:
+            case `${PAPER}-${ROCK}`:
+            case `${PAPER}-${SPOCK}`:
+            case `${SPOCK}-${ROCK}`:
+            case `${SPOCK}-${SCISSORS}`:
+            case `${LIZARD}-${PAPER}`:
+            case `${LIZARD}-${SPOCK}`:
+                result = 'COMPUTER WIN'
+                break;
+            case `${ROCK}-${PAPER}`:
+            case `${ROCK}-${SPOCK}`:
+            case `${SCISSORS}-${ROCK}`:
+            case `${SCISSORS}-${SPOCK}`:
+            case `${PAPER}-${SCISSORS}`:
+            case `${PAPER}-${LIZARD}`:
+            case `${SPOCK}-${PAPER}`:
+            case `${SPOCK}-${LIZARD}`:
+            case `${LIZARD}-${ROCK}`:
+            case `${LIZARD}-${SCISSORS}`:
+                result = 'YOU WIN ☺️🥇 🤖💔'
+                break;
+            default:
+                result = 'SOMETHING WRONG. TRY AGAIN.'
+        }
+
+        output.innerHTML = result;
+    })
 }
